b934369f1c3b1adebec1b6ea04ad1891dd482a3f
bruno.bortolotti <bruno@folhavitoria.com.br>
2017-11-11 01:41:13 -0200

    Changed the include method e use cases

--- lib/elastify.rb
+++ lib/elastify.rb
@@ -1,5 +1,7 @@
-require "elastify/version"
-require "elastify/active_record_extensions"
+require 'elastify/version'
+require 'elastify/active_record_extensions'
+require 'elastify/model'
+require 'elastify/mapping'
 
 module Elastify
     
@@ -7,14 +9,14 @@ module Elastify
         def configure &block
             mappings = {}
             configs = OpenStruct.new({
-                base_url: "http://localhost:9200",
-                mappings_path: Rails.root.join("config/elastify/mappings")
+                base_url: 'http://localhost:9200',
+                mappings_path: Rails.root.join('config/elastify/mappings')
             })
             block.call(configs) if block_given?
             dir = configs.mappings_path
             if Dir.exist?(dir)
                 Dir.glob("#{dir}/*.json") do |file_path|
-                    mappings[File.basename(file_path, ".json")] = JSON.parse(File.read(file_path))
+                    mappings[File.basename(file_path, '.json')] = JSON.parse(File.read(file_path))
                 end
             end
             Rails.application.config.elastify = {
